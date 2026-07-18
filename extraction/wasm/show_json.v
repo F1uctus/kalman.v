f94e259df193b6c82111ff6238ddbded8b152c86
Ilya I. Nikitin <ilya.i.nikitin@proton.me>
2026-07-18 14:54:23 +0300

    wasm: десятичный форматтер Q и комбинаторы JSON

---
+++ extraction/wasm/show_json.v
@@ -0,0 +1,39 @@
+(*
+  Печать рациональных чисел десятичной дробью и сборка документа JSON
+  для драйвера на WebAssembly. Значение Q выводится с фиксированным
+  числом дробных знаков (20), чего достаточно для точного восстановления
+  числа двойной точности при чтении в Typst. Матрицы и массивы собираются
+  простыми строковыми комбинаторами.
+*)
+Set Warnings "-all".
+From Stdlib Require Import ZArith QArith List Strings.String Strings.Ascii.
+From mathcomp.boot Require Import all_boot.
+From CoqEAL Require Import refinements seqmx.
+From KalmanC Require Import show. (* string_of_Z, nl, bytes_of_string *)
+
+Local Open Scope string_scope.
+
+Fixpoint frac_digits (p : nat) (rem den : Z) : string :=
+  match p with
+  | O => ""
+  | S p' =>
+      let r10 := (rem * 10)%Z in
+      let d := (r10 / den)%Z in
+      let r := (r10 mod den)%Z in
+      String (ascii_of_nat (48 + Z.to_nat d)) (frac_digits p' r den)
+  end.
+
+Definition q_dec (p : nat) (q : Q) : string :=
+  let n := Qnum q in
+  let d := Zpos (Qden q) in
+  let s := if (n <? 0)%Z then "-" else "" in
+  let a := Z.abs n in
+  s ++ string_of_Z (a / d)%Z ++ "." ++ frac_digits p (a mod d)%Z d.
+
+Definition jnum (q : Q) : string := q_dec 20 q.
+Definition jarr (xs : list string) : string := "[" ++ String.concat "," xs ++ "]".
+Definition jrow (r : list Q) : string := jarr (map jnum r).
+Definition jmat (m : @seqmx Q) : string := jarr (map jrow m).
+Definition jobj (kvs : list (string * string)) : string :=
+  "{" ++ String.concat ","
+        (map (fun kv => """" ++ fst kv ++ """:" ++ snd kv) kvs) ++ "}".
